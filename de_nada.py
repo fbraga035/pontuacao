470c3a87ea224ae92eb2178ecd2abbdd410df24e
fbraga035 <67554612+fbraga035@users.noreply.github.com>
2023-11-25 14:06:09 -0300

    Update de_nada.py

--- de_nada.py
+++ de_nada.py
@@ -1,15 +1,4 @@
-from flask import Flask, jsonify
+def teste():
+    print('a')
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, Bubble!'
-
-@app.route('/api/exemplo', methods=['GET'])
-def exemplo():
-    data = {'mensagem': 'Esta é uma resposta da API Python'}
-    return jsonify(data)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+teste()
