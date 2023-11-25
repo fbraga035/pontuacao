9cd28c96bc504e812bc6589882fe7db41bc40bf2
fbraga035 <67554612+fbraga035@users.noreply.github.com>
2023-11-25 14:16:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def teste():
-    print('a')
+from flask import Flask
 
-teste()
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
