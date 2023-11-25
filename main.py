999f162a9d4c338a393cc45cef6d799f5563ccb0
fbraga035 <67554612+fbraga035@users.noreply.github.com>
2023-11-25 15:03:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-from flask import Flask
-from waitress import serve
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    # Use o Waitress para servir o aplicativo
-    serve(app, host="0.0.0.0", port=5000)
+@app.get("/")
+def read_root():
+    return {"message": "Hello World!"}
