255ddc0212521d24ed0819978b16bcb20d0bedd4
fbraga035 <67554612+fbraga035@users.noreply.github.com>
2023-11-25 14:51:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use o Waitress para servir o aplicativo
+    serve(app, host="0.0.0.0", port=5000)
